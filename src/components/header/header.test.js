babc1a1814c3f0b883a5c5563682707c228eef5b
nabilgj <nabilgj.bnm@gmail.com>
2021-02-23 13:31:17 -0500

    header component and test

---
+++ src/components/header/header.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+
+import { findByTestAttr } from "../../../Utils";
+
+const setUp = (props = {}) => {
+  const component = shallow(<Header {...props} />);
+  return component;
+};
+
+describe("Header component", () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it("should render with out errors", () => {
+    const wrapper = findByTestAttr(component, "headerComponent");
+    expect(wrapper.length).toBe(1);
+  });
+
+  it("should render a logo", () => {
+    const logo = findByTestAttr(component, "logoImg");
+    expect(logo.length).toBe(1);
+  });
+});
